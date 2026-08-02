6388297a6f5dc40b21db17c7a7332a78bb19a248
Velvet-Application <sh96hv64dj@privaterelay.appleid.com>
2026-08-02 20:32:25 +0200

    fix(test-agents): respecter les identités autorisées

--- infra/supabase/migrations/0035_internal_test_agent_demographics.sql
+++ infra/supabase/migrations/0035_internal_test_agent_demographics.sql
@@ -10,15 +10,15 @@ as $$
 begin
   update public.individual_profiles ip
   set gender_identity = case
-    when new.slug = 'clara-mathieu' and ip.member_slot = 'partner_a' then 'femme'
-    when new.slug = 'clara-mathieu' and ip.member_slot = 'partner_b' then 'homme'
-    when new.slug = 'lea-nord' and ip.member_slot = 'individual' then 'femme'
-    when new.slug = 'sophie-thomas' and ip.member_slot = 'partner_a' then 'femme'
-    when new.slug = 'sophie-thomas' and ip.member_slot = 'partner_b' then 'homme'
-    when new.slug = 'maxime-lille' and ip.member_slot = 'individual' then 'homme'
-    when new.slug = 'nina-lucas' and ip.member_slot = 'partner_a' then 'femme'
-    when new.slug = 'nina-lucas' and ip.member_slot = 'partner_b' then 'homme'
-    when new.slug = 'camille-bxl' and ip.member_slot = 'individual' then 'non-binaire'
+    when new.slug = 'clara-mathieu' and ip.member_slot = 'partner_a' then 'Femme'
+    when new.slug = 'clara-mathieu' and ip.member_slot = 'partner_b' then 'Homme'
+    when new.slug = 'lea-nord' and ip.member_slot = 'individual' then 'Femme'
+    when new.slug = 'sophie-thomas' and ip.member_slot = 'partner_a' then 'Femme'
+    when new.slug = 'sophie-thomas' and ip.member_slot = 'partner_b' then 'Homme'
+    when new.slug = 'maxime-lille' and ip.member_slot = 'individual' then 'Homme'
+    when new.slug = 'nina-lucas' and ip.member_slot = 'partner_a' then 'Femme'
+    when new.slug = 'nina-lucas' and ip.member_slot = 'partner_b' then 'Homme'
+    when new.slug = 'camille-bxl' and ip.member_slot = 'individual' then 'Personne non binaire'
     else ip.gender_identity
   end
   where ip.profile_id = new.profile_id;
@@ -39,15 +39,15 @@ execute function public.sync_internal_test_agent_demographics();
 -- Backfill the six profiles that may already exist in staging.
 update public.individual_profiles ip
 set gender_identity = case
-  when a.slug = 'clara-mathieu' and ip.member_slot = 'partner_a' then 'femme'
-  when a.slug = 'clara-mathieu' and ip.member_slot = 'partner_b' then 'homme'
-  when a.slug = 'lea-nord' and ip.member_slot = 'individual' then 'femme'
-  when a.slug = 'sophie-thomas' and ip.member_slot = 'partner_a' then 'femme'
-  when a.slug = 'sophie-thomas' and ip.member_slot = 'partner_b' then 'homme'
-  when a.slug = 'maxime-lille' and ip.member_slot = 'individual' then 'homme'
-  when a.slug = 'nina-lucas' and ip.member_slot = 'partner_a' then 'femme'
-  when a.slug = 'nina-lucas' and ip.member_slot = 'partner_b' then 'homme'
-  when a.slug = 'camille-bxl' and ip.member_slot = 'individual' then 'non-binaire'
+  when a.slug = 'clara-mathieu' and ip.member_slot = 'partner_a' then 'Femme'
+  when a.slug = 'clara-mathieu' and ip.member_slot = 'partner_b' then 'Homme'
+  when a.slug = 'lea-nord' and ip.member_slot = 'individual' then 'Femme'
+  when a.slug = 'sophie-thomas' and ip.member_slot = 'partner_a' then 'Femme'
+  when a.slug = 'sophie-thomas' and ip.member_slot = 'partner_b' then 'Homme'
+  when a.slug = 'maxime-lille' and ip.member_slot = 'individual' then 'Homme'
+  when a.slug = 'nina-lucas' and ip.member_slot = 'partner_a' then 'Femme'
+  when a.slug = 'nina-lucas' and ip.member_slot = 'partner_b' then 'Homme'
+  when a.slug = 'camille-bxl' and ip.member_slot = 'individual' then 'Personne non binaire'
   else ip.gender_identity
 end
 from public.internal_test_agents a
