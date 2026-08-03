3e7d3356914f067efdc4471907741dacaea7d5eb
Velvet-Application <sh96hv64dj@privaterelay.appleid.com>
2026-08-03 11:57:51 +0200

    test(beta): isolate recipe service worker

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -9,6 +9,7 @@ export default defineConfig({
   reporter: [['list'], ['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
     baseURL: 'http://127.0.0.1:4173',
+    serviceWorkers: 'block',
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure'
   },
